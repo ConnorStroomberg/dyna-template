7c052e50a4d7f3994f6c5ce11433c9cb0bd13389
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-03-02 16:32:09 +0100

    working example

--- app.js
+++ app.js
@@ -8,19 +8,32 @@
     var dynamic = {
         functional: true,
         template: '#dynamic',
-        props: ['template'],
+        props: ['template', 'template-data'],
         render: function (h, context) {
             const myTemplate = context.props.template;
-            const component = myTemplate ? { template: myTemplate } : Loading;
-            return h(component)
+            const myData = context.props.templateData;
+            console.log(myData.name)
+            if(!myTemplate) {
+                return h(Loading)
+            } else {
+                return h({
+                    template: myTemplate,
+                    data: function () {
+                        return myData
+                    }
+                })
+            }
         }
     };
 
     new Vue({
         el: "#app",
         data: {
-            html: '<div id="mount-point"><span>First template</span></div>',
-            index: 0
+            html: '<div id="mount-point"><span>First template</span><div>{{name}}</div><div>{{age}}</div></div>',
+            person: {
+                name: 'Connor',
+                age: 37
+            }
         },
         components: {
             dynamic: dynamic
