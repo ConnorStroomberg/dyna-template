ccbdb36b168fdb23980c1ae1fad59dd895bb8498
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-03-02 16:01:49 +0100

    setup linus borg example https://forum.vuejs.org/t/vue-compile-what-is-staticrenderfns-render-vs-staticrenderfns/3950/12

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 (function () {
-    console.log('Hello App!')
+    console.log('Hello App!');
+
+    var Loading = {
+        template: '<div>Loading...</div>'
+    };
+
+    var dynamic = {
+        functional: true,
+        template: '#dynamic',
+        props: ['template'],
+        render: function (h, context) {
+            const myTemplate = context.props.template;
+            const component = myTemplate ? { template: myTemplate } : Loading;
+            return h(component)
+        }
+    };
+
+    new Vue({
+        el: "#app",
+        data: {
+            html: '<div id="mount-point"><span>First template</span></div>',
+            index: 0
+        },
+        components: {
+            dynamic: dynamic
+        }
+    });
 })();
