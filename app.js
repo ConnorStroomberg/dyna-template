d2b7d5831d609a1d47b6e48f105ef377d7800bbf
Mark-de-Haan <markdehaan90@gmail.com>
2018-03-03 12:06:56 +0100

    Updates: * Data editor * Entity example * Cleaned up code

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
 (function () {
-    console.log('Hello App!');
-
-    var Loading = {
-        template: '<div>Loading...</div>'
-    };
-
-    var dynamic = {
-        functional: true,
-        template: '#dynamic',
-        props: ['template', 'template-data'],
-        render: function (h, context) {
-            const myTemplate = context.props.template;
-            const myData = context.props.templateData;
-            console.log(myData.name)
-            if(!myTemplate) {
-                return h(Loading)
-            } else {
-                return h({
-                    template: myTemplate,
-                    data: function () {
-                        return myData
-                    }
-                })
-            }
-        }
-    };
-
-    new Vue({
-        el: "#app",
-        data: {
-            html: '<div id="mount-point"><span>First template</span><div>{{name}}</div><div>{{age}}</div></div>',
-            person: {
-                name: 'Connor',
-                age: 37
-            }
+  var entity = {
+    meta: {
+      id: 'example',
+      label: 'Example table',
+      attributes: [
+        {
+          id: 'name',
+          label: 'Name'
         },
-        components: {
-            dynamic: dynamic
+        {
+          id: 'age',
+          label: 'Age'
         }
-    });
-})();
+      ]
+    },
+    items: [
+      {
+        name: 'Connor',
+        age: 37
+      },
+      {
+        name: 'Mark',
+        age: 27
+      },
+      {
+        name: 'Tommy',
+        age: 30
+      }
+    ]
+  }
+
+  Vue.component('DynamicTemplate', {
+    functional: true,
+    template: '#dynamic-template',
+    props: ['template', 'entity'],
+    render: function (h, context) {
+      return h({
+        template: context.props.template,
+        data: function () {
+          return {
+            entity: JSON.parse(context.props.entity)
+          }
+        }
+      })
+    }
+  })
+
+  new Vue({
+    el: '#app',
+    data: {
+      template: '<div id="template-container">{{entity}}</div>',
+      entity: JSON.stringify(entity)
+    }
+  })
+})()
